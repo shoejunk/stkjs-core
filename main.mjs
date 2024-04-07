113b8655bb3997c2a6065a70a20c44a9961db4bd
Joseph Shunk <joseph.shunk@gmail.com>
2024-04-06 21:03:16 -0700

    Modernize Checklist

--- main.mjs
+++ main.mjs
@@ -1,11 +1,18 @@
 import UTIL from './lib/utilities.mjs';
 import EVENTS from './lib/events.mjs';
+import Checklist from './lib/checklist.mjs';
 
-export { UTIL as UTIL };
+export { UTIL, EVENTS, Checklist };
 
+// EVENTS
 UTIL.log('stkjs-core loaded');
-new EVENTS.EventHandler(['boop'], function requirementMet()
-{
-    UTIL.log('boop happened');
-});
+new EVENTS.EventHandler(['boop'], () => UTIL.log('boop happened'));
 EVENTS.fire('boop');
+
+// Checklist
+const checklist = new Checklist(() => UTIL.log('check'));
+checklist.addEventRequirement('bedoop');
+checklist.addEventRequirement('borp');
+EVENTS.fire('bedoop');
+EVENTS.fire('borp');
+
