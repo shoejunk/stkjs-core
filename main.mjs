80167ee7144c3539624eff06a245269e7ea7820f
Joseph Shunk <joseph.shunk@gmail.com>
2024-04-06 19:30:43 -0700

    Added some core libraries.

--- main.mjs
+++ main.mjs
@@ -1,5 +1,11 @@
 import UTIL from './lib/utilities.mjs';
+import EVENTS from './lib/events.mjs';
 
 export { UTIL as UTIL };
 
 UTIL.log('stkjs-core loaded');
+new EVENTS.EventHandler(['boop'], function requirementMet()
+{
+    UTIL.log('boop happened');
+});
+EVENTS.fire('boop');
